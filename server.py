ccf9e37bc7bfd98c553f28bda08a3928d984361c
Moamen Shabana <imoamen@users.noreply.github.com>
2024-04-24 13:58:40 +0300

    multiple fixes

--- server.py
+++ server.py
@@ -1,20 +1,8 @@
 import socket
 from _thread import start_new_thread
-import sys
 
 server = "127.0.0.1"
 port = 5555
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    print('Socket error occurred:', str(e))
-
-s.listen(2)
-print('waiting for a connection, Server Started')
-
 
 def threaded_client(conn):
     conn.send(str.encode('connected'))
@@ -38,9 +26,22 @@ def threaded_client(conn):
 
     print('lost connection')
     conn.close() 
- 
+
+def start_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((server, port))
+    except socket.error as e:
+        print('Socket error occurred:', str(e))
+
+    s.listen(2)
+    print('waiting for a connection, Server Started')
+    
     while True:
         conn, addr = s.accept()
-        print('conected to:', addr)
+        print('connected to:', addr)
 
         start_new_thread(threaded_client, (conn,))
+
+if __name__ == "__main__":
+    start_server()
