7e9a1a0d788ba543d76066ca431ee2b6fad455e7
Moamen Shabana <imoamen@users.noreply.github.com>
2024-04-30 19:29:12 +0300

    loop to retaking user input

--- code_snippet.py
+++ code_snippet.py
@@ -22,14 +22,29 @@ def get_operation():
             print("Invalid operation type...please enter (+, -, /, *)")
 
 
-
+def is_user_wants_to_continue():
+    while True:
+        try:
+            user_input = input("Welcome to the math calculator, Do you want to continue? (y/n): ")
+            if user_input in ("y", "n"):
+                if user_input == "n":
+                    print("Thank you for using the calculator!")
+                    return False
+                elif user_input == "y":
+                    return True
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid input...please enter (y, n)")
 
 if __name__ == "__main__":
-    first_number = get_user_input()
-    second_number = get_user_input()
-    operation = get_operation()
-    result = eval(f"{first_number} {operation} {second_number}")
-    print("Result is:", result) if result is not None else print("Unexpected Error")
+    while is_user_wants_to_continue():
+
+        first_number = get_user_input()
+        second_number = get_user_input()
+        operation = get_operation()
+        result = eval(f"{first_number} {operation} {second_number}")
+        print("Result is:", result) if result is not None else print("Unexpected Error")
 
         
         
